7df319a2e53e316cb445918c8720306ba83d20a0
nishant <nishant.singh87@gmail.com>
2015-06-20 19:50:05 +0530

    adding files

---
+++ js/app.js
@@ -0,0 +1,6 @@
+(function() {
+  'use strict';
+
+  var app = {};
+  window.app = app
+}).call(this);
---
+++ test/specs/app-spec.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict"
+
+  QUnit.module("MyApp", {
+    setup: function () {}
+  });
+
+  QUnit.test("Should do something", function (assert) {
+    assert.ok(true, "should do this.");
+  });
+
+}).call(this);
